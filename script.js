5cee18a837ea99191ba659194082d0fb7386ac2a
DKFIT <kgirdziunas0@gmail.com>
2023-11-16 09:45:58 +0200

    a

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+import { masyvas } from "./script2.js";
+console.log(masyvas);
+
+import { sudetis } from "./script2.js";
+console.log(sudetis(10));
+
+import favFood from "./script2.js";
+console.log(favFood);
+
+import { isStr } from "./script2.js";
+console.log(isStr("miaumiau"));
+console.log(isStr(123));
+console.log(isStr(false));
+
+import { start, end, total } from "./script2.js";
+console.log("funkcija uztruko: " + total + "milisekundziu");
+
+labas();
+
+function labas() {
+	console.log("pasisveikinimas");
+}
+console.log(typeof labas);
---
+++ script2.js
@@ -0,0 +1,16 @@
+export const masyvas = [4, 8, 12, 20, 60];
+
+export function sudetis(sk) {
+	return sk + sk;
+}
+
+const favFood = ["banana", "latte", "kebab", "pizza"];
+export default favFood;
+
+export const isStr = (value) => typeof value == "string";
+
+const start = performance.now();
+const end = performance.now();
+const total = start - end;
+export { start, end, total };
+
